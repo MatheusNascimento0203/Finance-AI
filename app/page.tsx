7a69a0bfa322a941baf945110585fc0de1b0d5f1
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-11-05 16:37:09 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>OI MATHEUS </h1>;
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">Hello Word </h1>;
+    </div>
+  );
 }
