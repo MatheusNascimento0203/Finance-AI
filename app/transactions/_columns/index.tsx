8cb519400c3cf78b41a4e8fbcf30919eefff8af6
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-11-06 17:02:51 -0300

    feat: add transaction type badge

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import TransactionTypebadge from "../_components/type-badge";
+
+export const transactionsColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => (
+      <TransactionTypebadge transaction={transaction} />
+    ),
+  },
+
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
