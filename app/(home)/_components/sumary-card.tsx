b25634a584de3b820e5bf715559873ba4d700fac
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-11-09 01:40:59 -0300

    feat: add transactions pie chart component

--- app/(home)/_components/sumary-card.tsx
+++ app/(home)/_components/sumary-card.tsx
@@ -17,7 +17,7 @@ const SumaryCard = ({
 }: SumaryCardProps) => {
   return (
     <>
-      <Card>
+      <Card className={`${size === "large" ? "bg-white bg-opacity-10" : " "}`}>
         <CardHeader className="flex-row items-center gap-4">
           {icon}
           <p
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,8 @@ import Navbar from "../_components/navbar";
 import SumaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
+import TransactionPierChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../data/get-dashboard";
 
 interface HomeProps {
   searchParams: { month: string };
@@ -22,6 +24,8 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect("?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <Navbar />
@@ -30,7 +34,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SumaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SumaryCards month={month} {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionPierChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
