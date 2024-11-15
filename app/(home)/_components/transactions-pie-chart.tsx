9851df2b8caf86025b107de2833b25448c4efcf7
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-11-15 19:13:03 -0300

    feat: add scroll in pie-chart  on smaller screen

--- app/(home)/_components/transactions-pie-chart.tsx
+++ app/(home)/_components/transactions-pie-chart.tsx
@@ -14,6 +14,7 @@ import { TransactionType } from "@prisma/client";
 import { TransactionPercentagePerType } from "@/app/data/get-dashboard/type";
 import { PiggyBankIcon, TrendingDownIcon, TrendingUpIcon } from "lucide-react";
 import PercentageItem from "./percentage-item";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
 
 const chartConfig = {
   [TransactionType.INVESTMENT]: {
@@ -54,44 +55,46 @@ const TransactionPierChart = ({
   ];
 
   return (
-    <Card className="flex flex-col p-6">
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
+    <ScrollArea className="rounded-md border">
+      <Card className="flex flex-col p-6">
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+          >
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={chartData}
+                dataKey="amount"
+                nameKey="type"
+                innerRadius={60}
+              />
+            </PieChart>
+          </ChartContainer>
+          <div className="space-y-3">
+            <PercentageItem
+              icon={<TrendingUpIcon size={16} className="text-primary" />}
+              title="Receita"
+              value={typesPercentage[TransactionType.DEPOSIT]}
             />
-            <Pie
-              data={chartData}
-              dataKey="amount"
-              nameKey="type"
-              innerRadius={60}
+            <PercentageItem
+              icon={<TrendingDownIcon size={16} className="text-danger" />}
+              title="Despesas"
+              value={typesPercentage[TransactionType.EXPENSE]}
             />
-          </PieChart>
-        </ChartContainer>
-        <div className="space-y-3">
-          <PercentageItem
-            icon={<TrendingUpIcon size={16} className="text-primary" />}
-            title="Receita"
-            value={typesPercentage[TransactionType.DEPOSIT]}
-          />
-          <PercentageItem
-            icon={<TrendingDownIcon size={16} className="text-danger" />}
-            title="Despesas"
-            value={typesPercentage[TransactionType.EXPENSE]}
-          />
-          <PercentageItem
-            icon={<PiggyBankIcon size={16} />}
-            title="Investido"
-            value={typesPercentage[TransactionType.INVESTMENT]}
-          />
-        </div>
-      </CardContent>
-    </Card>
+            <PercentageItem
+              icon={<PiggyBankIcon size={16} />}
+              title="Investido"
+              value={typesPercentage[TransactionType.INVESTMENT]}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </ScrollArea>
   );
 };
 
